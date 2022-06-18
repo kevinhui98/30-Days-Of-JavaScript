9d42ffe2017472e6abdbcee02a08f105bb679502
Kevin Hui <kevinhui98@gmail.com>
2022-06-18 14:19:37 -0400

    saving progress

---
+++ 07_Day_Functions/07_day_starter/scripts/day7.js
@@ -0,0 +1,13 @@
+// function fullname() {
+//     console.log('kevin hui')
+// }
+// fullname()
+function fullname(first, last) {
+    console.log(first + " " + last)
+}
+fullname('kevin', 'hui')
+function addNumbers(a, b) {
+    return a + b
+}
+console.log(addNumbers(2, 6))
+// function areaOfRectangle
